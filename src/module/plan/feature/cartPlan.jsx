67cecd75b136630e30a3f66f4bfb5f796263ef8d
somayeh124 <karimzadeh.somayeh1380@gmail.com>
2024-09-15 14:39:15 +0330

    calculate

--- src/module/plan/feature/cartPlan.jsx
+++ src/module/plan/feature/cartPlan.jsx
@@ -2,12 +2,10 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
 import PropTypes from 'prop-types';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle'; 
-import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
 import { useNavigate } from 'react-router-dom';
 
           
-const CartPlan = ({buoyancy,paymentPeriod,profit,picture,totalTime,fundedAmount,companyName,title,id,key,nominalPriceCertificate,applicantFundingPercentage,faraboursLink,symbol,marketer,remainingDays,activityField,planStatus,description}) => {
+const CartPlan = ({buoyancy,paymentPeriod,profit,picture,totalTime,fundedAmount,companyName,title,id,key,description}) => {
     const navigate = useNavigate ();
 
  
@@ -17,7 +15,7 @@ const CartPlan = ({buoyancy,paymentPeriod,profit,picture,totalTime,fundedAmount,
 
 
   return (
-    <div className="flex flex-wrap justify-center gap-6 bg-white p-4">
+    <div className="flex mt-24 flex-wrap justify-center gap-6 bg-white p-4">
      
         <div key={key} className="shadow-md w-72 p-4 mt-18 rounded-md border bg-white">
           <div className="relative h-40 w-full mb-4">
@@ -31,18 +29,19 @@ const CartPlan = ({buoyancy,paymentPeriod,profit,picture,totalTime,fundedAmount,
             </div>
           </div>
           <h3 className="font-bold text-lg mb-2 h-16">{title}</h3>
+          <h4 className="font-bold text-lg mb-2 h-16">{companyName}</h4>
           <p className="text-base text-gray-800 mb-4">پیش‌بینی میزان سود: <span className="text-sm text-gray-600 font-semibold">{profit}</span></p>
           <p className="text-base text-gray-800 mb-4"> مبلغ سرمایه‌گذاری: <span className="text-sm text-gray-600 font-semibold">{fundedAmount}</span></p>
-          <p className="text-base text-gray-800 mb-4">شروع: <span className="text-sm text-gray-600 font-semibold">{card.startDate}</span></p>
+          {/* <p className="text-base text-gray-800 mb-4">شروع: <span className="text-sm text-gray-600 font-semibold">{card.startDate}</span></p> */}
           <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4" style={{ description: 'ltr' }}>
-            {/* <div className="bg-blue-900 h-2.5 rounded-full" style={{ width: `${card.progress}%` }}></div> */}
+            <div className="bg-blue-900 h-2.5 rounded-full">{description}</div>
           </div>
           <div className="flex justify-between items-center text-sm text-gray-600 mb-4 mt-4">
-            <p>سرمایه‌گذاری شده: <span className="text-sm text-gray-700 font-semibold">{card.investedAmount}</span></p>
-            <div className="h-6 w-px bg-gray-400 mx-2"></div> 
-            <p>تعداد سرمایه‌گذاران: <span className="text-sm text-gray-700 font-semibold">{card.investors} نفر</span></p>
-            <div className="h-6 w-px bg-gray-400 mx-2"></div> 
-            <p>پیش‌بینی سود: <span className="font-semibold text-gray-700">{card.profitRate}</span></p>
+            <p> درصدشناوری: <span className="text-sm text-gray-700 font-semibold">{buoyancy}</span></p>
+            {/* <div className="h-6 w-px bg-gray-400 mx-2"></div>  */}
+            <p> دوره پرداخت: <span className="text-sm text-gray-700 font-semibold">{paymentPeriod} نفر</span></p>
+            {/* <div className="h-6 w-px bg-gray-400 mx-2"></div>  */}
+            <p>پیش‌بینی سود: <span className="font-semibold text-gray-700">{profit}</span></p>
           </div>
           <div className="flex justify-between items-center text-sm text-gray-600 mb-4">
             <p>{totalTime} روز تا پایان فرصت</p>
@@ -62,12 +61,16 @@ const CartPlan = ({buoyancy,paymentPeriod,profit,picture,totalTime,fundedAmount,
 
 CartPlan.propTypes = {
   id: PropTypes.number.isRequired,
-  status: PropTypes.string.isRequired,
-  expert: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  totalTime: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  createdDate: PropTypes.string.isRequired,
-  handledDate: PropTypes.string.isRequired,
-  trackingCode: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  key: PropTypes.number.isRequired,
+  profit: PropTypes.number.isRequired,
+  buoyancy: PropTypes.number.isRequired,
+  fundedAmount: PropTypes.number.isRequired,
+  companyName: PropTypes.string.isRequired,
+  paymentPeriod: PropTypes.number.isRequired,
 };
 
 export default CartPlan;
