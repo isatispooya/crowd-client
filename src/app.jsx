ece5ce7b0f2a0dc19568378a9278444a3d7aa458
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2024-09-16 15:14:01 +0330

    1234

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 /* eslint-disable unused-imports/no-unused-imports */
 
@@ -5,22 +6,31 @@ import { createTheme } from '@mui/material/styles';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ThemeProvider from 'src/theme';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
 import Router from 'src/routes/sections';
 import 'src/global.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
 
-
 export default function App() {
   const queryClient = new QueryClient();
   useScrollToTop();
 
+  const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <Router />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <CacheProvider value={cacheRtl}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <Router />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </CacheProvider>
   );
 }
-
