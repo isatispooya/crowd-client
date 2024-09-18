305c07540d8b347555ed39fb8a78d3baab3fb96b
somayeh124 <karimzadeh.somayeh1380@gmail.com>
2024-09-18 15:27:55 +0330

    end plan

--- src/module/plan/feature/partner.jsx
+++ src/module/plan/feature/partner.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import Loader from 'src/components/loader';
 import { useParams } from 'react-router-dom';
+import { FiDownload } from 'react-icons/fi';
+import { OnRun } from 'src/api/OnRun';
 import usepartner from '../service/use-participant';
 
 const Partner = () => {
@@ -15,19 +17,28 @@ const Partner = () => {
   if (isLoading) {
     return <Loader />;
   }
-
- 
-
   return (
-    <div className="flex flex-col gap-6 bg-white p-8 rounded-xl shadow-lg max-w-lg mx-auto">
+    <div className="flex flex-col gap-6 bg-gray-20 p-8 rounded-xl shadow-lg max-w-lg mx-auto">
       <h2 className="text-3xl items-center text-center font-bold text-gray-900 mb-6">دارایی ها</h2>
 
       <div className="grid grid-rows-1 gap-4">
-        <div className="bg-blue-100 p-4 rounded-lg shadow-sm">
+        <div className="bg-blue-100 flex justify-between p-4 rounded-lg shadow-sm">
           <p className="text-gray-700 font-semibold">
             تعداد گواهی خریداری شده:
-            <span className="text-blue-800 ml-2">{data[0].amount}</span>
           </p>
+          <p className="text-blue-800 text-base font-semibold">{data[0].amount}</p>
+
+        </div>
+        <div className="flex gap-4 justify-center">
+          <a
+            href={`${OnRun}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 flex items-center hover:text-blue-800 text-sm font-medium transition-colors duration-200 ease-in-out"
+          >
+            دانلود فایل گواهی
+            <FiDownload className="w-5 h-5 ml-2" />
+          </a>
         </div>
       </div>
     </div>
--- src/module/plan/page/plan.jsx
+++ src/module/plan/page/plan.jsx
@@ -14,7 +14,7 @@ import Partnership from '../feature/partnership';
 
 const Plan = () => {
   const { id } = useParams();
-  const {  isLoading, error } = usePlan(id);
+  const { isLoading, error } = usePlan(id);
   const [activeTab, setActiveTab] = useState(0);
   if (isLoading) {
     return <Loader />;
@@ -23,10 +23,10 @@ const Plan = () => {
     return <div className="text-red-500">خطایی رخ داده است: {error.message}</div>;
   }
   return (
-    <div className="w-full max-w-4xl mx-auto mt-8 p-4">
+    <div className="w-full max-w-4xl mx-auto p-4">
       <div className="border-b-2 border-gray-200 mb-6">
         <ul className="flex justify-center">
-        <li className="mr-4">
+          <li className="mr-4">
             <button
               className={`py-2 px-4 ${
                 activeTab === 0
@@ -35,8 +35,8 @@ const Plan = () => {
               }`}
               onClick={() => setActiveTab(0)}
             >
-                          مشارکت
-                </button>
+              مشارکت
+            </button>
           </li>
           <li className="mr-4">
             <button
@@ -67,8 +67,12 @@ const Plan = () => {
               className={`py-2 px-4 ${
                 activeTab === 3
                   ? 'text-blue-600 border-b-2 border-blue-600'
-                  : 'text-gray-500 hover:text-blue-600'}`}
-                  onClick={() => setActiveTab(3)}>نظرات کاربران    </button>
+                  : 'text-gray-500 hover:text-blue-600'
+              }`}
+              onClick={() => setActiveTab(3)}
+            >
+              نظرات کاربران{' '}
+            </button>
           </li>
           <li className="mr-4">
             <button
@@ -79,7 +83,7 @@ const Plan = () => {
               }`}
               onClick={() => setActiveTab(4)}
             >
-               مشخصات سرمایه گذارن    
+              مشخصات سرمایه گذارن
             </button>
           </li>
           <li className="mr-4">
@@ -91,7 +95,7 @@ const Plan = () => {
               }`}
               onClick={() => setActiveTab(5)}
             >
-               مستندات      
+              مستندات
             </button>
           </li>
           <li className="mr-4">
@@ -103,36 +107,30 @@ const Plan = () => {
               }`}
               onClick={() => setActiveTab(6)}
             >
-               تضامین      
+              تضامین
             </button>
           </li>
         </ul>
       </div>
       <div className="mt-6">
-      {activeTab === 0 && (
-        <div className='flex justify-between'><Partner /><Partnership /></div>
+        {activeTab === 0 && (
+          <div className="flex justify-evenly">
+            <div >
+              <Partner />
+            </div>
+            <div >
+              <Partnership />
+            </div>
+          </div>
         )}
-        {activeTab === 1 && (
-        <Descript/>
-        )}
+        {activeTab === 1 && <Descript />}
 
-        {activeTab === 2 && (
-          <Calculate/>
-        )}
-         {activeTab === 3 && (
-          <CommentForm/>
-        )}
-   
-         {activeTab === 4 && (
-          <InvestProfile/>
-        )}
-         {activeTab === 5 && (
-          <Documentation/>
-        )}
-         {activeTab === 6 && (
-          <Appendices/>
-        )}
-        
+        {activeTab === 2 && <Calculate />}
+        {activeTab === 3 && <CommentForm />}
+
+        {activeTab === 4 && <InvestProfile />}
+        {activeTab === 5 && <Documentation />}
+        {activeTab === 6 && <Appendices />}
       </div>
     </div>
   );
