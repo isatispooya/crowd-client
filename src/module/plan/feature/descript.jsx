56eba3cd856ba9cd4b6f86fb6253929f6954fa07
somayeh124 <karimzadeh.somayeh1380@gmail.com>
2024-10-02 11:45:24 +0330

    edit

--- src/module/plan/feature/descript.jsx
+++ src/module/plan/feature/descript.jsx
@@ -8,8 +8,8 @@ import useGetInformation from '../service/use-getinformtion';
 const Descript = () => {
   const { traceCode } = useParams();
   const { data, isPending, error } = useGetPlan(traceCode);
-  const { data:addinformtion ,isLoading:addloading } = useGetInformation(traceCode);
-  if (isPending||addloading) {
+  const { data: addinformtion, isLoading: addloading } = useGetInformation(traceCode);
+  if (isPending || addloading) {
     return <Loader />;
   }
 
@@ -28,17 +28,22 @@ const Descript = () => {
         
       </div> */}
       <div className="bg-gray-100 w-full mb-8 p-4 rounded-lg shadow-md">
-          <p className="text-gray-500">توضیحات</p>
-          <p className="text-lg text-gray-900 font-semibold">{data.persian_subject}</p>
-        </div>
+        <p className="text-gray-500">توضیحات</p>
+        <p className="text-lg text-gray-900 font-semibold break-words whitespace-normal">
+          {data.persian_subject}
+        </p>
+      </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">نام فارسی</p>
           <p className="text-lg text-gray-900 font-semibold">{data.persian_name}</p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-          <p className="text-gray-500">نماد  </p>
-          <p className="text-lg text-gray-900 font-semibold">{data.persoan_approved_symbol || 'نامشخص'}</p>
+          <p className="text-gray-500">نماد </p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {data.persoan_approved_symbol || 'نامشخص'}
+          </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">نام انگلیسی</p>
@@ -46,11 +51,15 @@ const Descript = () => {
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">نماد </p>
-          <p className="text-lg text-gray-900 font-semibold">{data.english_approved_symbol || 'نامشخص'}</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {data.english_approved_symbol || 'نامشخص'}
+          </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">سود </p>
-          <p className="text-lg text-gray-900 font-semibold">%{addinformtion.rate_of_return || 'نامشخص'}</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            %{addinformtion.rate_of_return || 'نامشخص'}
+          </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">عنوان گروه صنعت</p>
@@ -58,59 +67,81 @@ const Descript = () => {
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">عنوان زیر گروه صنعت</p>
-          <p className="text-lg text-gray-900 font-semibold">{data.sub_industry_group_description}</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {data.sub_industry_group_description}
+          </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">قیمت اسمی هر گواهی شراکت (ریال)</p>
-          <p className="text-lg text-gray-900 font-semibold">{formatNumber(data.unit_price)} ریال</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {formatNumber(data.unit_price)} ریال
+          </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">تعداد کل گواهی‌های شراکت قابل عرضه</p>
-          <p className="text-lg text-gray-900 font-semibold">{data.total_units}</p>
+          <p className="text-lg text-gray-900 font-semibold">{formatNumber(data.total_units)}</p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">تعداد گواهی شراکت متقاضی</p>
-          <p className="text-lg text-gray-900 font-semibold">{data.company_unit_counts}</p>
+          <p className="text-lg text-gray-900 font-semibold">{formatNumber(data.company_unit_counts)}</p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">مبلغ مورد نیاز (ریال)</p>
-          <p className="text-lg text-gray-900 font-semibold">{formatNumber(data.total_price)} ریال</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {formatNumber(data.total_price)} ریال
+          </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">عنوان نوع تامین مالی</p>
-          <p className="text-lg text-gray-900 font-semibold">{data.crowd_funding_type_description}</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {data.crowd_funding_type_description}
+          </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">حداقل مبلغ مورد نیاز جهت موفقیت تامین مالی (ریال)</p>
-          <p className="text-lg text-gray-900 font-semibold">{formatNumber(data.minimum_required_price)} ریال</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {formatNumber(data.minimum_required_price)} ریال
+          </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">حداقل مبلغ سرمایه‌گذاری برای تامین کننده حقیقی (ریال)</p>
-          <p className="text-lg text-gray-900 font-semibold">{formatNumber(data.real_person_minimum_availabe_price)} ریال</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {formatNumber(data.real_person_minimum_availabe_price)} ریال
+          </p>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">حداکثر مبلغ سرمایه‌گذاری برای تامین کننده حقیقی (ریال)</p>
-          <p className="text-lg text-gray-900 font-semibold">{formatNumber(data.real_person_maximum_available_price)} ریال</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {formatNumber(data.real_person_maximum_available_price)} ریال
+          </p>
         </div>
 
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">حداقل مبلغ سرمایه‌گذاری برای تامین کننده حقوقی (ریال)</p>
-          <p className="text-lg text-gray-900 font-semibold">{formatNumber(data.legal_person_minimum_availabe_price)||0} ریال</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {formatNumber(data.legal_person_minimum_availabe_price) || 0} ریال
+          </p>
         </div>
 
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">حداکثر مبلغ سرمایه‌گذاری برای تامین کننده حقوقی (ریال)</p>
-          <p className="text-lg text-gray-900 font-semibold">{formatNumber(data.legal_person_maximum_availabe_price) || 'نامشخص'}</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {formatNumber(data.legal_person_maximum_availabe_price) || 'نامشخص'}
+          </p>
         </div>
 
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">تاریخ شروع جمع آوری وجوه</p>
-          <p className="text-lg text-gray-900 font-semibold">{data.persian_suggested_underwiring_start_date}</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {data.persian_suggested_underwiring_start_date}
+          </p>
         </div>
 
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <p className="text-gray-500">تاریخ پایان جمع آوری وجوه</p>
-          <p className="text-lg text-gray-900 font-semibold">{data.persian_suggested_underwriting_end_date}</p>
+          <p className="text-lg text-gray-900 font-semibold">
+            {data.persian_suggested_underwriting_end_date}
+          </p>
         </div>
 
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
