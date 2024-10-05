39ad01b4e8bd633806b2e9b00e0dbecc9dc29b8b
somayeh124 <karimzadeh.somayeh1380@gmail.com>
2024-10-05 10:44:32 +0330

    responsive

--- src/module/plan/investor/feature/Investorprofile.jsx
+++ src/module/plan/investor/feature/Investorprofile.jsx
@@ -22,7 +22,8 @@ const InvestProfile = () => {
     <div className="max-w-7xl mx-auto p-4 bg-white rounded-lg shadow-md">
       <h1 className="text-xl font-bold mb-4 text-center">پروفایل سرمایه‌گذاران</h1>
 
-      <div className="overflow-x-auto">
+      {/* جدول برای دستگاه‌های بزرگتر */}
+      <div className="hidden sm:block overflow-x-auto">
         <table className="min-w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -64,6 +65,7 @@ const InvestProfile = () => {
         </table>
       </div>
 
+      {/* نمایش کارت برای موبایل */}
       <div className="grid grid-cols-1 gap-4 sm:hidden">
         {transactionData.map((item, index) => (
           <div key={index} className="bg-white p-4 rounded-lg shadow-md">
