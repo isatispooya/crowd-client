597d39cc1635af546eb14405e9c99cfd86c56380
somayeh124 <karimzadeh.somayeh1380@gmail.com>
2024-10-13 11:38:34 +0330

    chartlimit

--- src/module/plan/feature/descript.jsx
+++ src/module/plan/feature/descript.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unsafe-optional-chaining */
 import React from 'react';
 import { useParams } from 'react-router-dom';
@@ -6,14 +7,26 @@ import Loader from 'src/components/loader';
 import PropTypes from 'prop-types';
 import { OnRun } from 'src/api/OnRun';
 import ProgressLineChart from 'src/components/progressLine';
+import { GiSandsOfTime } from 'react-icons/gi';
+import {FiExternalLink,FiDollarSign,FiCalendar,FiFileText,FiFlag,FiHash} from 'react-icons/fi';
+import { FaRegMoneyBill1 } from 'react-icons/fa6';
+import { Divider } from '@mui/material';
 import useGetPlan from '../service/use-plan';
 import usePicure from '../service/use-picture';
 import DateDifference from './dateDifference';
+import ChartLimitInvest from './chartLimitInvest';
 
-const Field = ({ label, value, bold }) => (
-  <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-    <p className="text-gray-500">{label}</p>
-    <p className={`text-lg text-gray-900 ${bold ? 'font-semibold' : ''}`}>{value}</p>
+const Field = ({ label, value, bold, hasBackground, icon: Icon }) => (
+  <div className={`p-4 overflow-hidden ${hasBackground ? 'bg-gray-50 rounded-lg shadow-md' : ''}`}>
+    <div className="text-right mb-2 text-sm">
+      <p className="text-gray-400">{label}</p>
+    </div>
+    <div className="flex items-center justify-between relative">
+      <p className={`text-lg text-gray-900 ${bold ? 'font-semibold' : ''}`}>{value}</p>
+      {Icon && (
+        <Icon className="text-violet-300 w-16 h-16 ml-2 absolute left-[-30px] top-[-10px] rotate-[30deg]" />
+      )}
+    </div>
   </div>
 );
 
@@ -22,6 +35,7 @@ const Descript = () => {
   const { data, isPending, error } = useGetPlan(traceCode);
 
   const { data: picture, isLoading: loadingpicture } = usePicure(traceCode);
+ 
 
   if (isPending || loadingpicture || !data) {
     return <Loader />;
@@ -32,58 +46,76 @@ const Descript = () => {
   }
 
   const generalFields = [
-    { label: 'نماد (فارسی)', value: data.plan.persoan_approved_symbol || 'نامشخص' },
-    { label: 'نام شرکت', value: data.company[0].name },
-    { label: 'سود', value: `%${data.information_complete.rate_of_return / 100 || 'نامشخص'}` },
-    { label: 'عنوان گروه صنعت', value: data.plan.industry_group_description },
-    { label: 'عنوان زیر گروه صنعت', value: data.plan.sub_industry_group_description },
     {
-      label: 'قیمت اسمی هر گواهی شراکت (ریال)',
-      value: `${formatNumber(data.plan.unit_price)} ریال`,
+      label: 'نماد (فارسی)',
+      value: data.plan.persoan_approved_symbol || 'نامشخص',
+      icon: FiFileText,
     },
-    { label: 'تعداد کل گواهی‌های شراکت قابل عرضه', value: formatNumber(data.plan.total_units) },
-    { label: 'تعداد گواهی شراکت متقاضی', value: formatNumber(data.plan.company_unit_counts) },
-    { label: 'عنوان نوع تامین مالی', value: data.plan.crowd_funding_type_description },
+    { label: 'نام شرکت', value: data.company[0].name, icon: FiFileText },
+    {
+      label: 'سود',
+      value: `%${data.information_complete.rate_of_return || 'نامشخص'}`,
+      icon: FiDollarSign,
+    },
+    { label: 'عنوان گروه صنعت', value: data.plan.industry_group_description, icon: FiFileText },
+    {
+      label: 'عنوان زیر گروه صنعت',
+      value: data.plan.sub_industry_group_description,
+      icon: FiFileText,
+    },
     {
       label: 'تاریخ شروع جمع آوری وجوه',
       value: data.plan.persian_suggested_underwiring_start_date,
+      icon: FiCalendar,
     },
     {
       label: 'تاریخ پایان جمع آوری وجوه',
       value: data.plan.persian_suggested_underwriting_end_date,
+      icon: FiCalendar,
     },
-    { label: 'تاریخ شروع اجرای طرح', value: data.plan.persian_project_start_date },
-    { label: 'تاریخ پایان اجرای طرح', value: data.plan.persian_project_end_date },
-    { label: 'وضعیت پروژه', value: data.plan.project_status_description },
-    { label: 'مبلغ مورد نیاز (ریال)', value: `${formatNumber(data.plan.total_price)} ریال` },
+    {
+      label: 'قیمت اسمی هر گواهی شراکت (ریال)',
+      value: `${formatNumber(data.plan.unit_price)} ریال`,
+      icon: FaRegMoneyBill1,
+    },
+    {
+      label: 'تعداد کل گواهی‌های شراکت قابل عرضه',
+      value: formatNumber(data.plan.total_units),
+      icon: FiHash,
+    },
+    {
+      label: 'تعداد گواهی شراکت متقاضی',
+      value: formatNumber(data.plan.company_unit_counts),
+      icon: FiHash,
+    },
+    {
+      label: 'عنوان نوع تامین مالی',
+      value: data.plan.crowd_funding_type_description,
+      icon: FiFileText,
+    },
+    {
+      label: 'تاریخ شروع اجرای طرح',
+      value: data.plan.persian_project_start_date,
+      icon: FiCalendar,
+    },
+    { label: 'تاریخ پایان اجرای طرح', value: data.plan.persian_project_end_date, icon: FiCalendar },
+    { label: 'وضعیت پروژه', value: data.plan.project_status_description, icon: FiFlag },
+    {
+      label: 'مبلغ مورد نیاز (ریال)',
+      value: `${formatNumber(data.plan.total_price)} ریال`,
+      icon: FaRegMoneyBill1,
+    },
 
     {
       label: 'حداقل مبلغ مورد نیاز جهت موفقیت تامین مالی (ریال)',
       value: `${formatNumber(data.plan.minimum_required_price) || 0} ریال`,
+      icon: FaRegMoneyBill1,
     },
   ];
 
-  const Real = [
-    {
-      label: 'حداقل مبلغ سرمایه‌گذاری برای تامین کننده حقیقی (ریال)',
-      value: `${formatNumber(data.plan.real_person_minimum_availabe_price) || 0} ریال`,
-    },
-    {
-      label: 'حداکثر مبلغ سرمایه‌گذاری برای تامین کننده حقیقی (ریال)',
-      value: `${formatNumber(data.plan.real_person_maximum_available_price) || 0} ریال`,
-    },
-  ];
 
-  const Legal = [
-    {
-      label: 'حداقل مبلغ سرمایه‌گذاری برای تامین کننده حقوقی (ریال)',
-      value: `${formatNumber(data.plan.legal_person_minimum_availabe_price) || 0} ریال`,
-    },
-    {
-      label: 'حداکثر مبلغ سرمایه‌گذاری برای تامین کننده حقوقی (ریال)',
-      value: `${formatNumber(data.plan.legal_person_maximum_availabe_price ?? 0)} ریال`,
-    },
-  ];
+
+
 
   return (
     <div className="p-6 bg-white rounded-lg shadow-lg max-w-4xl mx-auto mt-4">
@@ -105,65 +137,67 @@ const Descript = () => {
           />
         )}
       </div>
-      <div className='flex justify-between'>
+      <div className="flex justify-between">
         <a
           href="https://cf.ifb.ir/home/viewproject"
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-500 underline hover:text-blue-700 flex items-center"
         >
-          مشاهده در فرابورس
+          مشاهده در فرابورس <FiExternalLink className="ml-2" />
         </a>
-        <div className="mt-6">
-        <DateDifference
-          startDate={data.plan.suggested_underwriting_start_date}
-          endDate={data.plan.suggested_underwriting_end_date}
-        />
-      </div>
+        <div className="mt-6 flex items-center">
+          <GiSandsOfTime className="text-yellow-600 w-6 h-6 ml-2" />
+          <DateDifference
+            startDate={data.plan.suggested_underwriting_start_date}
+            endDate={data.plan.suggested_underwriting_end_date}
+          />
+        </div>
       </div>
       <div className="w-full mb-8 p-4">
-        <p className="text-gray-500">توضیحات</p>
         <p className="text-base text-gray-700 font-medium break-words whitespace-normal">
           {data.plan.persian_subject}
         </p>
       </div>
-      <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
         {generalFields.map((field, index) => (
-          <Field key={index} label={field.label} value={field.value} />
+          <Field
+            key={index}
+            label={field.label}
+            value={field.value}
+            hasBackground
+            icon={field.icon}
+          />
         ))}
       </div>
-    
       <div className="mt-6 px-4">
-          <ProgressLineChart
-            progress={Math.round((data.information_complete.amount_collected_now / data.plan.total_price) * 100)}
-            label="تامین شده"
-          />
-          <p className="text-center text-sm  text-gray-800 mt-4">
-            مبلغ تامین شده: {formatNumber(data.information_complete.amount_collected_now ?? 0)} ریال
-          </p>
-        </div>
-   
-      <div className="flex p-6 gap-12">
-        <div className="flex-1">
-          <h3 className="text-gray-700 font-bold mb-4 text-lg">سرمایه‌گذاری حقیقی</h3>
-          <div className="grid grid-cols-1 gap-6 mb-8">
-            {Real.map((field, index) => (
-              <Field key={index} label={field.label} value={field.value} bold />
-            ))}
-          </div>
-        </div>
-
-        <div className="h-auto" />
-
-        <div className="flex-1">
-          <h3 className="text-gray-700 font-bold mb-4 text-lg">سرمایه‌گذاری حقوقی</h3>
-          <div className="grid grid-cols-1 gap-6">
-            {Legal.map((field, index) => (
-              <Field key={index} label={field.label} value={field.value} bold />
-            ))}
-          </div>
-        </div>
+        <ProgressLineChart
+          progress={Math.round(
+            (data.information_complete.amount_collected_now / data.plan.total_price) * 100
+          )}
+          label="تامین شده"
+        />
+        <p className="text-center text-sm  text-gray-800 mt-4">
+          مبلغ تامین شده: {formatNumber(data.information_complete.amount_collected_now ?? 0)} ریال
+        </p>
       </div>
+      <ChartLimitInvest 
+  priceMin={data.plan.real_person_minimum_availabe_price} 
+  priceMax={data.plan.real_person_maximum_available_price}
+  unit_price={data?.plan?.unit_price} 
+/>
+
+<Divider 
+  sx={{ 
+    borderBottomWidth: 3, marginY: 4,backgroundColor: 'gray'
+  }} 
+/>
+<ChartLimitInvest 
+  priceMin={data.plan.legal_person_minimum_availabe_price} 
+  priceMax={data.plan.legal_person_maximum_availabe_price}
+  unit_price={data?.plan?.unit_price} 
+/>
+
     </div>
   );
 };
