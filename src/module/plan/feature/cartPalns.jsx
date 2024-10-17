62f6d2f0ddee3a7de3825039d88229208ff2739a
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2024-10-17 10:04:34 +0330

    11

--- src/module/plan/feature/cartPalns.jsx
+++ src/module/plan/feature/cartPalns.jsx
@@ -27,63 +27,63 @@ const CartPlans = () => {
           filterStatusSecond.includes(item?.information_complete?.status_second)
         )
       : data;
-  return ( <>
+  return (
+    <>
       <div className="bg-gray-200 text-white rounded-t-md p-1 text-center ">
-    <h1 className="text-2xl  text-gray-700">طرح ها</h1>
-  </div>
-    <div className="flex justify-center items-center max-h-screen">
-      <div className="max-w-8xl w-full bg-white rounded-lg shadow-2xl p-2">
+        <h1 className="text-2xl  text-gray-700">طرح ها</h1>
+      </div>
+      <div className="flex justify-center items-center max-h-screen">
+        <div className="max-w-8xl w-full bg-white rounded-lg shadow-2xl p-2">
+          <div className="bg-gray-100 shadow-inner rounded-2xl text-white rounded-t-md  text-center ">
+            <div className="text-3xl font-bold text-gray-700">
+              <FilterPlans setFilterStatusSecond={setFilterStatusSecond} />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10 justify-center text-right">
+            {filteredPlans.length > 0 ? (
+              filteredPlans.map((item) => {
+                const persianCreationDate = item.plan?.creation_date
+                  ? moment(item.plan?.creation_date).locale('fa').format('YYYY/MM/DD')
+                  : 'تاریخ نامعتبر';
 
-        <div className="bg-gray-100 shadow-inner rounded-2xl text-white rounded-t-md  text-center ">
-          <div className="text-3xl font-bold text-gray-700">
-            <FilterPlans setFilterStatusSecond={setFilterStatusSecond} />
+                if (item?.information_complete?.status_show === true) {
+                  return (
+                    <div key={item.plan.id}>
+                      <CartPlan
+                        trace_code={item.plan.trace_code}
+                        persianName={item.plan.persian_name}
+                        englishName={item.plan.english_name}
+                        industryGroup={item.plan.industry_group_description}
+                        totalUnits={item.plan.total_units}
+                        totalPrice={item.plan.total_price}
+                        crowdFundingType={item.plan.crowd_funding_type_description}
+                        projectStatus={item.plan.project_status_description}
+                        settlementDescription={item.plan.settlement_description}
+                        realPersonMinPrice={item.plan.real_person_minimum_availabe_price}
+                        realPersonMaxPrice={item.plan.real_person_maximum_available_price}
+                        crowdFundingtypeDescription={item.plan.crowd_funding_type_description}
+                        creation_date={persianCreationDate}
+                        persoanApprovedSymbol={item.plan.persoan_approved_symbol}
+                        statusSecond={item?.information_complete?.status_second}
+                        amountCollectedNow={item?.information_complete?.amount_collected_now}
+                        rateOfReturn={item?.information_complete?.rate_of_return}
+                        company={item?.company[0]?.name}
+                        startDate={item.plan.suggested_underwriting_start_date}
+                        endDate={item.plan.suggested_underwriting_end_date}
+                        statusShow={item?.information_complete?.status_show}
+                      />
+                    </div>
+                  );
+                }
+                return null;
+              })
+            ) : (
+              <p>هیچ طرحی برای این فیلتر یافت نشد.</p>
+            )}
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 justify-center text-right">
-          {filteredPlans.length > 0 ? (
-            filteredPlans.map((item) => {
-              const persianCreationDate = item.plan?.creation_date
-                ? moment(item.plan?.creation_date).locale('fa').format('YYYY/MM/DD')
-                : 'تاریخ نامعتبر';
-
-              if (item?.information_complete?.status_show === true) {
-                return (
-                  <div key={item.plan.id}>
-                    <CartPlan
-                      trace_code={item.plan.trace_code}
-                      persianName={item.plan.persian_name}
-                      englishName={item.plan.english_name}
-                      industryGroup={item.plan.industry_group_description}
-                      totalUnits={item.plan.total_units}
-                      totalPrice={item.plan.total_price}
-                      crowdFundingType={item.plan.crowd_funding_type_description}
-                      projectStatus={item.plan.project_status_description}
-                      settlementDescription={item.plan.settlement_description}
-                      realPersonMinPrice={item.plan.real_person_minimum_availabe_price}
-                      realPersonMaxPrice={item.plan.real_person_maximum_available_price}
-                      crowdFundingtypeDescription={item.plan.crowd_funding_type_description}
-                      creation_date={persianCreationDate}
-                      persoanApprovedSymbol={item.plan.persoan_approved_symbol}
-                      statusSecond={item?.information_complete?.status_second}
-                      amountCollectedNow={item?.information_complete?.amount_collected_now}
-                      rateOfReturn={item?.information_complete?.rate_of_return}
-                      company={item?.company[0]?.name}
-                      startDate={item.plan.suggested_underwriting_start_date}
-                      endDate={item.plan.suggested_underwriting_end_date}
-                      statusShow={item?.information_complete?.status_show}
-                    />
-                  </div>
-                );
-              }
-              return null;
-            })
-          ) : (
-            <p>هیچ طرحی برای این فیلتر یافت نشد.</p>
-          )}
-        </div>
       </div>
-    </div></>
-
+    </>
   );
 };
 
