23c65c0462d3b4f24a500e045428019a00927ef4
نام کاربری شما < >
2025-03-05 13:42:42 +0330

    افزودن پاسخگویی به موبایل و بهبود استپر در صفحه جزئیات کارت‌ها

--- src/module/createPlan/feature/InformationRecording.jsx
+++ src/module/createPlan/feature/InformationRecording.jsx
@@ -1,7 +1,28 @@
-import { TextField, Typography, Grid, Paper } from '@mui/material';
+import { useState } from 'react';
+import { TextField, Typography, Grid, Paper, Button, IconButton, Box } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const InformationRecording = () => {
-  // رنگ‌های آبی پاستیلی
+  const [files, setFiles] = useState({
+    logo: null,
+    financial: null,
+    credit: null,
+  });
+
+  const handleFileChange = (event, type) => {
+    setFiles({
+      ...files,
+      [type]: event.target.files[0],
+    });
+  };
+
+  const handleFileRemove = (type) => {
+    setFiles({
+      ...files,
+      [type]: null,
+    });
+  };
+
   const pastelBlue = {
     light: '#E6F4FF',
     main: '#B3E0FF',
@@ -26,10 +47,8 @@ const InformationRecording = () => {
         border: `1px solid ${pastelBlue.main}`,
         position: 'relative',
         overflow: 'hidden',
-        transition: 'all 0.3s ease-in-out',
         '&:hover': {
           boxShadow: '0 15px 35px rgba(149, 157, 165, 0.2)',
-          transform: 'translateY(-3px)',
         },
       }}
     >
@@ -49,7 +68,7 @@ const InformationRecording = () => {
             left: '10%',
             width: '80%',
             height: '3px',
-            background: `linear-gradient(90deg, ${pastelBlue.main}, ${pastelBlue.dark})`,
+            background: `linear-gradient(90deg, ${pastelBlue.main}, ${pastelBlue.contrastText})`,
             borderRadius: '2px',
           },
         }}
@@ -74,7 +93,7 @@ const InformationRecording = () => {
               borderWidth: '1.5px',
             },
             '&:hover fieldset': {
-              borderColor: pastelBlue.dark,
+              borderColor: pastelBlue.contrastText,
             },
             '&.Mui-focused fieldset': {
               borderColor: pastelBlue.dark,
@@ -84,7 +103,7 @@ const InformationRecording = () => {
           '& .MuiInputLabel-root': {
             color: pastelBlue.contrastText,
             '&.Mui-focused': {
-              color: pastelBlue.dark,
+              color: pastelBlue.contrastText,
             },
           },
         }}
@@ -114,32 +133,60 @@ const InformationRecording = () => {
           >
             لوگو شرکت
           </Typography>
-          <TextField
-            type="file"
-            fullWidth
-            variant="outlined"
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                background: '#FFFFFF',
-                borderRadius: '10px',
-                transition: 'transform 0.2s',
-                '&:hover': {
-                  transform: 'translateY(-2px)',
+          {!files.logo ? (
+            <TextField
+              type="file"
+              fullWidth
+              variant="outlined"
+              onChange={(e) => handleFileChange(e, 'logo')}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  background: '#FFFFFF',
+                  borderRadius: '10px',
+                  transition: 'transform 0.2s',
+                  '&:hover': {
+                    transform: 'translateY(-2px)',
+                  },
+                  '& fieldset': {
+                    borderColor: pastelBlue.main,
+                    borderWidth: '1.5px',
+                  },
+                  '&:hover fieldset': {
+                    borderColor: pastelBlue.dark,
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: pastelBlue.dark,
+                    borderWidth: '2px',
+                  },
                 },
-                '& fieldset': {
-                  borderColor: pastelBlue.main,
-                  borderWidth: '1.5px',
-                },
-                '&:hover fieldset': {
-                  borderColor: pastelBlue.dark,
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: pastelBlue.dark,
-                  borderWidth: '2px',
-                },
-              },
-            }}
-          />
+              }}
+            />
+          ) : (
+            <div>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                  backgroundColor: '#FFFFFF',
+                  padding: '10px',
+                  borderRadius: '10px',
+                }}
+              >
+                <a
+                  href={URL.createObjectURL(files.logo)}
+                  download={files.logo.name}
+                  style={{ textDecoration: 'none', color: 'inherit' }}
+                >
+                  <Typography variant="body1">{files.logo.name}</Typography>
+                </a>
+                <IconButton onClick={() => handleFileRemove('logo')}>
+                  <DeleteIcon />
+                </IconButton>
+              </Box>
+            </div>
+          )}
         </Grid>
 
         <Grid item xs={12} md={6}>
@@ -165,32 +212,54 @@ const InformationRecording = () => {
           >
             صورت مالی
           </Typography>
-          <TextField
-            type="file"
-            fullWidth
-            variant="outlined"
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                background: '#FFFFFF',
-                borderRadius: '10px',
-                transition: 'transform 0.2s',
-                '&:hover': {
-                  transform: 'translateY(-2px)',
+          {!files.financial ? (
+            <TextField
+              type="file"
+              fullWidth
+              variant="outlined"
+              onChange={(e) => handleFileChange(e, 'financial')}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  background: '#FFFFFF',
+                  borderRadius: '10px',
+                  transition: 'transform 0.2s',
+                  '&:hover': {
+                    transform: 'translateY(-2px)',
+                  },
+                  '& fieldset': {
+                    borderColor: pastelBlue.main,
+                    borderWidth: '1.5px',
+                  },
+                  '&:hover fieldset': {
+                    borderColor: pastelBlue.dark,
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: pastelBlue.dark,
+                    borderWidth: '2px',
+                  },
                 },
-                '& fieldset': {
-                  borderColor: pastelBlue.main,
-                  borderWidth: '1.5px',
-                },
-                '&:hover fieldset': {
-                  borderColor: pastelBlue.dark,
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: pastelBlue.dark,
-                  borderWidth: '2px',
-                },
-              },
-            }}
-          />
+              }}
+            />
+          ) : (
+            <div>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                  backgroundColor: '#FFFFFF',
+                  padding: '10px',
+                  borderRadius: '10px',
+                }}
+              >
+                <Typography variant="body1">{files.financial.name}</Typography>
+                <IconButton onClick={() => handleFileRemove('financial')}>
+                  <DeleteIcon />
+                </IconButton>
+              </Box>
+            </div>
+          )}
         </Grid>
 
         <Grid item xs={12} md={6}>
@@ -216,32 +285,71 @@ const InformationRecording = () => {
           >
             گزارش اعتبارسنجی
           </Typography>
-          <TextField
-            type="file"
+          {!files.credit ? (
+            <TextField
+              type="file"
+              fullWidth
+              variant="outlined"
+              onChange={(e) => handleFileChange(e, 'credit')}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  background: '#FFFFFF',
+                  borderRadius: '10px',
+                  transition: 'transform 0.2s',
+                  '&:hover': {
+                    transform: 'translateY(-2px)',
+                  },
+                  '& fieldset': {
+                    borderColor: pastelBlue.main,
+                    borderWidth: '1.5px',
+                  },
+                  '&:hover fieldset': {
+                    borderColor: pastelBlue.dark,
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: pastelBlue.dark,
+                    borderWidth: '2px',
+                  },
+                },
+              }}
+            />
+          ) : (
+            <div>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                  backgroundColor: '#FFFFFF',
+                  padding: '10px',
+                  borderRadius: '10px',
+                }}
+              >
+                <Typography variant="body1">{files.credit.name}</Typography>
+                <IconButton onClick={() => handleFileRemove('credit')}>
+                  <DeleteIcon />
+                </IconButton>
+              </Box>
+            </div>
+          )}
+        </Grid>
+
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
             fullWidth
-            variant="outlined"
             sx={{
-              '& .MuiOutlinedInput-root': {
-                background: '#FFFFFF',
-                borderRadius: '10px',
-                transition: 'transform 0.2s',
-                '&:hover': {
-                  transform: 'translateY(-2px)',
-                },
-                '& fieldset': {
-                  borderColor: pastelBlue.main,
-                  borderWidth: '1.5px',
-                },
-                '&:hover fieldset': {
-                  borderColor: pastelBlue.dark,
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: pastelBlue.dark,
-                  borderWidth: '2px',
-                },
+              mt: 3,
+              backgroundColor: pastelBlue.contrastText,
+              '&:hover': {
+                backgroundColor: pastelBlue.dark,
               },
             }}
-          />
+          >
+            ارسال اطلاعات
+          </Button>
         </Grid>
       </Grid>
     </Paper>
--- src/module/createPlan/pages/cardsDetail.jsx
+++ src/module/createPlan/pages/cardsDetail.jsx
@@ -3,48 +3,123 @@ import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
+import MobileStepper from '@mui/material/MobileStepper';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Paper from '@mui/material/Paper';
 import InformationRecording from '../feature/InformationRecording';
 
-const steps = [
-  'ثبت شرکت ',
-  'ثبت هیئت مدیره ',
-  'قرارداد عاملیت',
-  'اطلاعات تکمیلی',
-  'قرار داد اجرایی',
-];
+const steps = ['ثبت شرکت', 'ثبت هیئت مدیره', 'قرارداد عاملیت', 'اطلاعات تکمیلی', 'قرارداد اجرایی'];
 
 const CardsDetail = () => {
   const [activeStep, setActiveStep] = React.useState(0);
+  const isMobile = useMediaQuery('(max-width: 600px)');
 
-  const isStepFailed = (step) => {
-    return step === 1;
+  const handleNext = () => {
+    if (activeStep < steps.length - 1) setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    if (activeStep > 0) setActiveStep(activeStep - 1);
+  };
+
+  const handleStepClick = (index) => {
+    setActiveStep(index);
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const labelProps = {};
-          if (isStepFailed(index)) {
-            labelProps.optional = (
-              <Typography variant="caption" color="error">
-                تکمیل کنید
-              </Typography>
-            );
-
-            labelProps.error = true;
-          }
-
-          return (
-            <Step key={label}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
+    <Box
+      sx={{ width: '100%', p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      {!isMobile && (
+        <Stepper activeStep={activeStep} alternativeLabel sx={{ width: '100%', mb: 3 }}>
+          {steps.map((label, index) => (
+            <Step key={label} onClick={() => handleStepClick(index)}>
+              <StepLabel>{label}</StepLabel>
             </Step>
-          );
-        })}
-      </Stepper>
+          ))}
+        </Stepper>
+      )}
 
-      <Box sx={{ mt: 2 }}>{activeStep === 0 && <InformationRecording />}</Box>
+      {isMobile && (
+        <Paper
+          elevation={3}
+          sx={{
+            width: '100%',
+            p: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            borderRadius: 2,
+            bgcolor: '#fff',
+            color: 'white',
+          }}
+        >
+          <Typography variant="body1" sx={{ mb: 1, fontWeight: 'bold', color: 'black' }}>
+            مرحله فعلی:
+          </Typography>
+          <Select
+            value={activeStep}
+            onChange={(event) => handleStepClick(event.target.value)}
+            displayEmpty
+            fullWidth
+            variant="outlined"
+            sx={{
+              bgcolor: 'white',
+              color: 'black',
+              fontWeight: 'bold',
+              borderRadius: 2,
+              mb: 1,
+            }}
+          >
+            {steps.map((label, index) => (
+              <MenuItem key={index} value={index}>
+                {label}
+              </MenuItem>
+            ))}
+          </Select>
+
+          <MobileStepper
+            variant="dots"
+            steps={steps.length}
+            position="static"
+            activeStep={activeStep}
+            sx={{ bgcolor: 'transparent' }}
+            nextButton={
+              <Button
+                size="small"
+                onClick={handleNext}
+                disabled={activeStep === steps.length - 1}
+                sx={{ color: 'black' }}
+              >
+                بعدی
+              </Button>
+            }
+            backButton={
+              <Button
+                size="small"
+                onClick={handleBack}
+                disabled={activeStep === 0}
+                sx={{ color: 'black' }}
+              >
+                قبلی
+              </Button>
+            }
+          />
+        </Paper>
+      )}
+
+      <Box sx={{ mt: 3, textAlign: 'center', width: '100%' }}>
+        {activeStep === 0 && <InformationRecording />}
+        {activeStep > 0 && (
+          <Typography variant="h6" sx={{ fontWeight: 'bold', color: 'primary.dark' }}>
+            محتوای مرحله {steps[activeStep]}
+          </Typography>
+        )}
+      </Box>
     </Box>
   );
 };
